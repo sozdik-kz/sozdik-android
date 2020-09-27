40c22606ae6240cb82155b4bc574f6b0f6dc76a1
Askar Syzdykov <askar.syzdykov@btsdigital.kz>
2020-09-27 21:37:10 +0600

    Fix Profile persistence issue

--- app/src/main/java/kz/sozdik/profile/di/ProfileModule.kt
+++ app/src/main/java/kz/sozdik/profile/di/ProfileModule.kt
@@ -3,20 +3,24 @@ package kz.sozdik.profile.di
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kz.sozdik.profile.data.api.ProfileApi
 import kz.sozdik.profile.data.gateway.ProfileDbGateway
 import kz.sozdik.profile.data.gateway.ProfileRestGateway
 import kz.sozdik.profile.domain.ProfileLocalGateway
 import kz.sozdik.profile.domain.ProfileRemoteGateway
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 abstract class ProfileModule {
 
     @Binds
+    @Singleton
     abstract fun bindProfileRemoteGateway(repository: ProfileRestGateway): ProfileRemoteGateway
 
     @Binds
+    @Singleton
     abstract fun bindProfileLocalGateway(repository: ProfileDbGateway): ProfileLocalGateway
 
     @Module
